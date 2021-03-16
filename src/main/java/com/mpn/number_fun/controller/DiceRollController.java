5c0b41bbff22430ace9e6527af6cd9858bac1689
Matt Newbold <matt.newbold208@gmail.com>
2021-03-16 16:02:34 -0500

    fixed hyperlink

--- src/main/java/com/mpn/number_fun/controller/DiceRollController.java
+++ src/main/java/com/mpn/number_fun/controller/DiceRollController.java
@@ -1,9 +1,10 @@
 package com.mpn.number_fun.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class DiceRollController {
-    @GetMapping()
+    @GetMapping("diceroll")
     public String roller() {
         return "dice_roll";
     }
